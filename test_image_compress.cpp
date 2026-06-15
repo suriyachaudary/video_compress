de4093c03bda1e3f00ad2cafcac298d24259e327
Suriya Chaudary <suriyachaudaryth@gmail.com>
2026-06-15 08:46:08 +0700

    removed libeigen

--- test_image_compress.cpp
+++ test_image_compress.cpp
@@ -11,7 +11,8 @@ using Eigen::MatrixXd;
 
 int main(int argc, char **argv)
 {
-	printf("image path: %s\n", argv[1]);
+	char *image_path = argv[1];
+	printf("image path: %s\n", image_path);
 	MatrixXd m(2, 2);
   	m(0, 0) = 3;
   	m(1, 0) = 2.5;
