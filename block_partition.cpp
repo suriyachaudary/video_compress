cba74d396ca9fccaa13acb459dd3beb5ca114987
Suriya Chaudary <suriyachaudaryth@gmail.com>
2026-06-22 11:24:17 +0700

    quad tree

--- block_partition.cpp
+++ block_partition.cpp
@@ -6,7 +6,12 @@ is greater than threshold the block is split into K blocks.
 is greater than threshold the block is split into 2x2 blocks.
 Set threshold to 0 for lossless compression [1]. 
 Quadtree speed up processing (intersection and union) of masks [1].
-3. use weighted average [2]. Split block in order based on their standard deviation [2].
+3. use weighted average to compute standard deviation [2]. Split block in order based on their standard deviation [2].
+	a. Initialize root node at depth 0 with roi (0, 0, w, h) and error sum = standard deviation * node area. 
+	b. Push the root node into heap with score = -standard deviation * (node area ** 0.25)
+	c. Compute color of the node as mean color and standard deviation from histogram of the node.
+	d. check if the node is leaf node i.e., w <= 4 or h<=4
+4. append meshgrid (code from gemini) to image to know the coordinate of the block
 
 Reference 
 1. https://en.wikipedia.org/wiki/Quadtree
@@ -15,6 +20,31 @@ Reference
 #include<cmath>
 using namespace std;
 using namespace cv;
+
+class Quadtree2{
+	float block_mean=0, block_std_dev = 0, threshold = 10;
+	int block_width = 0, block_height = 0;
+	Mat block, coords; Rect region_in_image;
+
+	Quadtree2()
+	{
+		imshow("block", block);
+		waitKey(0);
+
+		Scalar mean, standard_deviation;
+
+		meanStdDev(block, mean, standard_deviation);
+		block_mean = (mean[0]*0.1140 + mean[1]*0.5870 + mean[2]*0.2989);
+		block_std_dev = sqrt((standard_deviation[0]*standard_deviation[0]*0.1140 +
+	 					standard_deviation[1]*standard_deviation[1]*0.5870 +
+	  					standard_deviation[2]*standard_deviation[2]*0.2989));
+
+		if(block_width > 4 && block_height > 4 && block_std_dev > threshold){
+
+			/*split the block*/
+		}
+	}
+};
 
 class Quadtree{
 
@@ -35,30 +65,29 @@ public:
 	{
 		quad_tree_image = quad_tree_image_temp;
 		Rect roi = Rect(0, 0, img->cols, img->rows);
+		if(img->rows > 4 && img-> cols > 4)
+		{
+			if(quad == 0)
+			{
+				roi = Rect(0, 0, img->cols/2, img->rows/2);
+			}
+			else if(quad == 1)
+			{
+				roi = Rect(img->cols/2, 0, img->cols/2, img->rows/2);
+			}
+			else if(quad == 2)
+			{
+				roi = Rect(0, img->rows/2, img->cols/2, img->rows/2);
+			}
+			else if(quad == 3)
+			{
+				roi = Rect(img->cols/2, img->rows/2, img->cols/2, img->rows/2);
+			}
 
-		if(quad == 0)
-		{
-			roi = Rect(0, 0, img->cols/2, img->rows/2);
-		}
-		else if(quad == 1)
-		{
-			roi = Rect(img->cols/2, 0, img->cols/2, img->rows/2);
-		}
-		else if(quad == 2)
-		{
-			roi = Rect(0, img->rows/2, img->cols/2, img->rows/2);
-		}
-		else if(quad == 3)
-		{
-			roi = Rect(img->cols/2, img->rows/2, img->cols/2, img->rows/2);
-		}
+			Mat block = img->clone()(roi);
 
-		Mat block = img->clone()(roi);
-
-		if(roi.width > 1 && roi.height > 1)
-		{
 			meanStdDev(block, mean, standard_deviation);
-			image_mean = (mean[0] + mean[1] + mean[2])/3;
+			image_mean = (mean[0]*0.1140 + mean[1]*0.5870 + mean[2]*0.2989);
 			image_std = sqrt((standard_deviation[0]*standard_deviation[0]*0.1140 +
 		 					standard_deviation[1]*standard_deviation[1]*0.5870 +
 		  					standard_deviation[2]*standard_deviation[2]*0.2989));
@@ -72,33 +101,35 @@ public:
 
 			if (image_std > threshold)
 			{
-				line(quad_tree_image, Point(roi.width/2, 0.0),
-			 		Point(roi.width/2, roi.height),
+				line(quad_tree_image, Point((roi.width)/2, roi.y),
+			 		Point((roi.width)/2, roi.height),
 			  		Scalar(255, 255, 255));
 			
-				line(quad_tree_image, Point(0.0, roi.height/2),
-			 		Point(roi.width, roi.height/2),
+				line(quad_tree_image, Point(roi.x, (roi.height)/2),
+			 		Point(roi.width, (roi.height)/2),
 			  		Scalar(255, 255, 255));
-				
+					printf("mean : %f, standard deviation : %f\n", image_mean, image_std);
+		
+			printf("image resolution %dx%d at %dx%dx%dx%d\n", img->rows, img->cols, roi.x, roi.y, roi.width, roi.height);
+			rectangle(quad_tree_image, roi, Scalar(255,0,255));
 
 				imshow("quad tree image", quad_tree_image);
 				waitKey(1);
-				imshow("blocks", block);
-				waitKey(0);
 				northwest = new Quadtree(&block, quad_tree_image, threshold, quad = 0);
 				northeast = new Quadtree(&block, quad_tree_image, threshold, quad = 1);
 				southwest = new Quadtree(&block, quad_tree_image, threshold, quad = 2);
 				southeast = new Quadtree(&block, quad_tree_image, threshold, quad = 3);
 			}
+			
 		}
 			else
 			{
-				printf("mean : %f, standard deviation : %f\n", image_mean, image_std);
+				// printf("mean : %f, standard deviation : %f\n", image_mean, image_std);
 		
-			printf("image resolution %dx%d\n", img->rows, img->cols);
-				// imshow("blocks", block);
-				// waitKey(1);
-				// put block for processing
+			// printf("image resolution %dx%d at %dx%dx%dx%d\n", img->rows, img->cols, roi.x, roi.y, roi.width, roi.height);
+			
+				imshow("blocks", *img);
+				waitKey(1);
 			}
 	}
 
