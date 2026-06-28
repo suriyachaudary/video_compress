84c8e5516a6e93912504d9cdd6a9ae062475348b
Suriya Chaudary <suriyachaudaryth@gmail.com>
2026-06-28 19:03:07 +0700

    filter

--- filter.cpp
+++ filter.cpp
@@ -1,6 +1,6 @@
 /* For each pixel, find 2 basis pixels each from the
  previous row and previous column of the pixel.
- Encode possible pairs of bases (b, a) (b, f),..., (b, u),..., (d, p), ..., (e, u)
+ Encode possible pairs of bases (b, a),..., (b, f),..., (b, u),..., (d, p), ..., (e, u)
  as 37 lines at angle between 0 and 180 degeree.
 
 a b c d e
@@ -10,6 +10,12 @@ p q r s t
 u v w x y
 
 Use multi threading to speed up and process all pixels in all blocks in parallel.
+Bit desciption. Each pixel is represented by 10 bits indicating the selected line from 37 lines using 6 bits
+and cosine distance from the line in 4 bits. Another 10 bits is used for encoding residual.
+0. start processing from (2,2) onwards
+1. for each pixel do
+	a. find possible bases of pixel pairs from previous row and column
+	b. 
 
 Reference
 1. libpng
